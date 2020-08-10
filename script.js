3104ee8e4ce5a692a36d89677465c09271c2ce2c
Felipinhov <felipinhov199@gmail.com>
2020-08-10 01:08:49 -0300

    Adicionado imagens e foi centralizado dentro da div a imagem

---
+++ script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERROR] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'centro')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'menino1.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovemM1.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homem1.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso1.png')
+            }
+
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', 'menina1.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'jovemF1.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'mulher1.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosa1.png')
+            }
+        }
+
+        res.innerHTML = `É um(a) ${genero} de ${idade} anos.`
+        res.appendChild(img)
+    }
+}
